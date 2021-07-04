4e4f08cfec455596880e2c7f52470b139fcbb0ce
RamonPG <ramonpg14@outlook.com>
2021-07-04 14:35:57 +0200

    create order

--- server.js
+++ server.js
@@ -37,5 +37,38 @@ app.delete("/api/products/:id", async(req, res) => {
     res.send(deletedProduct);
 });
 
+const Order = mongoose.model("order", new mongoose.Schema({
+    _id:{
+        type: String,
+        default: shortid.generate,
+    },
+    email: String,
+    name:String,
+    address: String,
+    total: Number,
+    cartItems: [{
+        _id: String,
+        tittle: String,
+        price: Number,
+        count: Number,
+    },],
+}, {
+    timestamps: true,
+}));
+
+app.post("/api/orders", async(req, res) => {
+    if(
+        !req.body.name ||
+        !req.body.email ||
+        !req.body.address ||
+        !req.body.total ||
+        !req.body.cartItems
+        ) {
+            return res.send({message:"Todos los campos son requeridos"});
+        }
+        const order = await Order(req.body).save();
+        res.send(order);
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log("serve at http://localhost:5000"));
